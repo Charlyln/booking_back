4a761d40b22070c28dbdb3ca31a39633d8941b51
Charlyln <charleshenry.lenue>
2020-07-22 11:06:29 +0200

    create models folder and suer model

---
+++ models/user.model.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+
+const sequelizeInstance = require("../sequelize");
+
+const User = sequelizeInstance.define("User", {
+  uuid: {
+    type: Sequelize.UUID,
+    allowNull: false,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4,
+  },
+  pseudo: Sequelize.STRING,
+  avatar: Sequelize.STRING,
+});
+
+module.exports = User;
