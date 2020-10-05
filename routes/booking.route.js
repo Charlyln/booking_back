25518f83e8fa277f96d91cc037064979a0efad0a
Charlyln <charleshenry.lenue>
2020-10-05 15:36:20 +0200

    change eslint format

--- routes/booking.route.js
+++ routes/booking.route.js
@@ -1,36 +1,36 @@
-const express = require("express");
-const bookings = express.Router();
-const Booking = require("../models/booking.model");
-const User = require("../models/user.model");
+const express = require('express')
+const bookings = express.Router()
+const Booking = require('../models/booking.model')
+const User = require('../models/user.model')
 
-bookings.get("/", async (req, res) => {
+bookings.get('/', async (req, res) => {
   try {
-    const bookings = await Booking.findAll({ include: [{ model: User }] });
-    res.status(200).json(bookings);
+    const bookings = await Booking.findAll({ include: [{ model: User }] })
+    res.status(200).json(bookings)
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json(err)
   }
-});
+})
 
-bookings.post("/", async (req, res) => {
-  const { TravelUuid, UserUuid, startDate, endDate, accepted } = req.body;
+bookings.post('/', async (req, res) => {
+  const { TravelUuid, UserUuid, startDate, endDate, accepted } = req.body
   try {
     const bookings = await Booking.create({
       TravelUuid,
       UserUuid,
       startDate,
       endDate,
-      accepted,
-    });
-    res.status(201).json(bookings);
+      accepted
+    })
+    res.status(201).json(bookings)
   } catch (err) {
-    res.status(422).json(err);
+    res.status(422).json(err)
   }
-});
+})
 
-bookings.put("/:uuid", async (req, res) => {
-  const uuid = req.params.uuid;
-  const { TravelUuid, UserUuid, startDate, endDate, accepted } = req.body;
+bookings.put('/:uuid', async (req, res) => {
+  const uuid = req.params.uuid
+  const { TravelUuid, UserUuid, startDate, endDate, accepted } = req.body
   try {
     const bookings = await Booking.update(
       {
@@ -38,24 +38,24 @@ bookings.put("/:uuid", async (req, res) => {
         UserUuid,
         startDate,
         endDate,
-        accepted,
+        accepted
       },
       { where: { uuid } }
-    );
-    res.status(204).json(bookings);
+    )
+    res.status(204).json(bookings)
   } catch (err) {
-    res.status(422).json(err);
+    res.status(422).json(err)
   }
-});
+})
 
-bookings.delete("/:id", async (req, res) => {
-  const id = req.params.id;
+bookings.delete('/:uuid', async (req, res) => {
+  const uuid = req.params.uuid
   try {
-    await Booking.destroy({ where: { id } });
-    res.status(204).send("Votre Booking a été supprimé");
+    await Booking.destroy({ where: { uuid } })
+    res.status(204).send('Votre Booking a été supprimé')
   } catch (err) {
-    res.status(422).json(err);
+    res.status(422).json(err)
   }
-});
+})
 
-module.exports = bookings;
+module.exports = bookings
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,74 +1,74 @@
-const express = require("express");
-const users = express.Router();
-const User = require("../models/user.model");
-const Likes = require("../models/like.model");
+const express = require('express')
+const users = express.Router()
+const User = require('../models/user.model')
+const Likes = require('../models/like.model')
+const Booking = require('../models/booking.model')
 
-
-users.get("/", async (req, res) => {
+users.get('/', async (req, res) => {
   try {
-    const users = await User.findAll();
-    res.status(200).json(users);
+    const users = await User.findAll()
+    res.status(200).json(users)
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json(err)
   }
-});
+})
 
-users.get("/:uuid", async (req, res) => {
-  const uuid = req.params.uuid;
+users.get('/:uuid', async (req, res) => {
+  const uuid = req.params.uuid
   try {
     const user = await User.findOne({
-      include: [{ model: Likes }],
+      include: [{ model: Likes }, { model: Booking }],
       where: {
-        uuid,
-      },
-    });
-    res.status(200).json(user);
+        uuid
+      }
+    })
+    res.status(200).json(user)
   } catch (error) {
     res.status(400).json({
-      status: "error",
-      message: "Invalid request",
-    });
+      status: 'error',
+      message: 'Invalid request'
+    })
   }
-});
+})
 
-users.post("/", async (req, res) => {
-  const { pseudo, avatar } = req.body;
+users.post('/', async (req, res) => {
+  const { pseudo, avatar } = req.body
   try {
     const user = await User.create({
       pseudo,
-      avatar,
-    });
-    res.status(201).json(user);
+      avatar
+    })
+    res.status(201).json(user)
   } catch (err) {
-    res.status(422).json(err);
+    res.status(422).json(err)
   }
-});
+})
 
-users.put("/:uuid", async (req, res) => {
-  const uuid = req.params.uuid;
-  const { pseudo, avatar } = req.body;
+users.put('/:uuid', async (req, res) => {
+  const uuid = req.params.uuid
+  const { pseudo, avatar } = req.body
   try {
     const user = await User.update(
       {
         pseudo,
-        avatar,
+        avatar
       },
       { where: { uuid } }
-    );
-    res.status(204).json(user);
+    )
+    res.status(204).json(user)
   } catch (err) {
-    res.status(422).json(err);
+    res.status(422).json(err)
   }
-});
+})
 
-users.delete("/:uuid", async (req, res) => {
-  const uuid = req.params.uuid;
+users.delete('/:uuid', async (req, res) => {
+  const uuid = req.params.uuid
   try {
-    await User.destroy({ where: { uuid } });
-    res.status(204).send("Votre compte a été supprimé");
+    await User.destroy({ where: { uuid } })
+    res.status(204).send('Votre compte a été supprimé')
   } catch (err) {
-    res.status(422).json(err);
+    res.status(422).json(err)
   }
-});
+})
 
-module.exports = users;
+module.exports = users
